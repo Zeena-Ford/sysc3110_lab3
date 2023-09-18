009dccba73011a4b0c6a9146251613d2a9ba5ef7
Zeena <zinaford.ottawa@gmail.com>
2023-09-17 21:09:07 -0400

    I made a buddyInfo object and added it to addressBook; I then removed it.

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,7 +1,24 @@
+import java.util.ArrayList;
+
+
+
 public class AddressBook {
-    }
 
-
+    ArrayList<BuddyInfo> list = new ArrayList<>();
     public static void main(String[] args) {
-        System.out.println("Address Book");
+        BuddyInfo buddy = new BuddyInfo("Zeena", "greenland street", 613);
+        AddressBook addressBook = new AddressBook();
+        addressBook.addBuddy(buddy);
+        addressBook.removeBuddy(buddy);
     }
+
+    public void addBuddy(BuddyInfo addBud){
+
+        list.add(addBud);
+    }
+
+    public void removeBuddy(BuddyInfo removeBud){
+        list.remove(removeBud);
+    }
+
+}
