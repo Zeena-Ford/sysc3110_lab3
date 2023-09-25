3d90c1f9faf3730cc11ded62544d9bfca35c3953
Zeena <zinaford.ottawa@gmail.com>
2023-09-24 23:08:13 -0400

    commit for buddyinfo.

---
+++ src/BuddyInfo.java
@@ -0,0 +1,38 @@
+public class BuddyInfo {
+
+    private String name;
+
+    private String address;
+
+    private int phoneNumber;
+
+    public static void main(String[] args) {
+
+        BuddyInfo bud = new BuddyInfo("Zeena", "greenland street", 613);
+        System.out.println("Hello " + bud.getName());
+        
+    }
+
+    public BuddyInfo(String name, String address, int phoneNumber){
+        this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+
+    }
+
+    public BuddyInfo(){
+
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getAddress(){
+        return address;
+    }
+
+    public int getPhoneNumber(){
+        return phoneNumber;
+    }
+}
